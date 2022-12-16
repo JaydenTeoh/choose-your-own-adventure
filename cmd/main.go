07b6685768e88c7cec24cb9bfcd70568d6bb9650
Jayden Teoh <t3ohjingxiang@gmail.com>
2022-12-17 02:16:42 +0800

    write http.handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
-	"cyoa/story"
-	"encoding/json"
+	cyoa "cyoa/story"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "port to start the CYOA application")                   //generate a port number to listen for later
 	fileName := flag.String("file", "gopher.json", "the JSON file to generate CYOA story") //let user choose which json file to generate storybook from
 	flag.Parse()                                                                           //allow program to access flag val
 	fmt.Printf("Using the story in %s.\n", *fileName)
 
 	f, err := os.Open(*fileName) //open the json file and return a pointer to an os.File which implements io.Reader
+	checkErr(err)
+
+	story, err := cyoa.JsonStory(f) //decode the json file using io.Reader and return a Story map with accessible struct values
+	checkErr(err)
+
+	h := cyoa.HttpHandler(story) //return makeshift http.Handler interface that implements ServeHttp function that allow us to handle requests and write story intro to response body
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h)) //listen for port 3000 and use h to handler http requests
+}
+
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-
-	d := json.NewDecoder(f) //return a decoder that reads from the os.File
-	var story story.Story
-	if err := d.Decode(&story); err != nil { //Decode reads the JSON-encoded value from f and stores it in story map.
-		panic(err)
-	}
-
-	fmt.Printf("%+v\n", story)
 }
--- story/story.go
+++ story/story.go
@@ -1,5 +1,66 @@
 package story
 
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl)) //template.Must ensures that we only return if html can be compiled else it will panic with the err
+}
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Choose Your Own Adventure</title>
+</head>
+
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+    </ul>
+</body>
+
+</html>
+`
+
+func HttpHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"]) //http.ResponseWriter is an io.Writer => can use it in Template.Execute to write to response body
+	if err != nil {
+		panic(err)
+	}
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r) //return a decoder that reads from the os.File
+	var story Story
+	if err := d.Decode(&story); err != nil { //Decode reads the JSON-encoded value from f and stores it in story map.
+		return nil, err
+	}
+	return story, nil
+}
+
 type Story map[string]Chapter
 
 type Chapter struct {
