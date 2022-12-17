3fd9f61aec1e20e837b78f2e4cb344414489db53
Jayden Teoh <t3ohjingxiang@gmail.com>
2022-12-17 23:59:34 +0800

    style html and allow functional options

--- cmd/main.go
+++ cmd/main.go
@@ -21,10 +21,94 @@ func main() {
 	story, err := cyoa.JsonStory(f) //decode the json file using io.Reader and return a Story map with accessible struct values
 	checkErr(err)
 
+	// tpl := template.Must(template.New("").Parse(customTmpl))
+
 	h := cyoa.HttpHandler(story) //return makeshift http.Handler interface that implements ServeHttp function that allow us to write story intro to initial response body and also handle future http requests
+	mux := http.NewServeMux()
+	mux.Handle("/", h)
 	fmt.Printf("Starting the server on port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h)) //listen for port 3000 and use h to handler http requests
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux)) //listen for port 3000 and use h to handler http requests
 }
+
+// func pathFn(r *http.Request) string {
+// 	path := strings.TrimSpace(r.URL.Path)
+// 	if path == "/story" || path == "/story/" || path == "/" {
+// 		path = "/story/intro"
+// 	}
+// 	return path[len("/story/"):]
+// }
+
+// var customTmpl = `
+// <!DOCTYPE html>
+// <html lang="en">
+
+// <head>
+//     <meta charset="UTF-8">
+//     <title>Choose Your Own Adventure</title>
+// 	<style>
+// 		body {
+// 			font-family: helvetica, arial;
+// 		}
+
+// 		h1 {
+// 			text-align: center;
+// 			position: relative;
+// 		}
+
+// 		.page {
+// 			width: 80%;
+// 			max-width: 500px;
+// 			margin: auto;
+// 			margin-top: 40px;
+// 			margin-bottom: 40px;
+// 			padding: 80px;
+// 			background: #FFFCF6;
+// 			border: 1px solid #eee;
+// 			box-shadow: 0 10px 6px -6px #777
+// 		}
+
+// 		ul {
+// 			border-top: 1px dotted #ccc;
+// 			padding-top: 10px;
+// 			-webkit-padding-start: 0;
+// 		}
+
+// 		li {
+// 			padding-top: 10px;
+// 		}
+
+// 		a,
+// 		a:visited {
+// 			text-decoration: none;
+// 			color: #6295b5;
+// 		}
+// 		a:active,
+// 		a:hover {
+// 			color: #7792a2;
+// 		}
+
+// 		p {
+// 			text-indent: 1em;
+// 		}
+//    </style>
+// </head>
+
+// <body>
+// 	<section class="page">
+// 		<h1>{{.Title}}</h1>
+// 		{{range .Paragraphs}}
+// 		<p>{{.}}</p>
+// 		{{end}}
+// 		<ul>
+// 			{{range .Options}}
+// 			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+// 			{{end}}
+// 		</ul>
+// 	</section>
+// </body>
+
+// </html>
+// `
 
 func checkErr(err error) {
 	if err != nil {
--- story/story.go
+++ story/story.go
@@ -22,41 +22,113 @@ var defaultHandlerTmpl = `
 <head>
     <meta charset="UTF-8">
     <title>Choose Your Own Adventure</title>
+	<style>
+		body {
+			font-family: helvetica, arial;
+		}
+	
+		h1 {
+			text-align: center;
+			position: relative;
+		}
+	
+		.page {
+			width: 80%;
+			max-width: 500px;
+			margin: auto;
+			margin-top: 40px;
+			margin-bottom: 40px;
+			padding: 80px;
+			background: #FFFCF6;
+			border: 1px solid #eee;
+			box-shadow: 0 10px 6px -6px #777
+		}
+
+		ul {
+			border-top: 1px dotted #ccc;
+			padding-top: 10px;
+			-webkit-padding-start: 0;
+		}
+
+		li {
+			padding-top: 10px;
+		}
+
+		a,
+		a:visited {
+			text-decoration: none;
+			color: #6295b5;
+		}
+		a:active,
+		a:hover {
+			color: #7792a2;
+		}
+
+		p {
+			text-indent: 1em;
+		}
+   </style>
 </head>
 
 <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-    <p>{{.}}</p>
-    {{end}}
-    <ul>
-        {{range .Options}}
-        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-        {{end}}
-    </ul>
+	<section class="page">
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+		<p>{{.}}</p>
+		{{end}}
+		<ul>
+			{{range .Options}}
+			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+		</ul>
+	</section>
 </body>
 
 </html>
 `
 
-func HttpHandler(s Story) http.Handler {
-	return handler{s}
+type HandlerOpt func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOpt {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func WithPathFn(pathFn func(r *http.Request) string) HandlerOpt {
+	return func(h *handler) {
+		h.pathFn = pathFn
+	}
+}
+
+func HttpHandler(s Story, opts ...HandlerOpt) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
 }
 
 type handler struct {
-	s Story
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path) //get the URL Path of the http requests with any whitespaces removed
 	if path == "" || path == "/" {
 		path = "/intro" //if response has no URL path, assume user is starting story from scratch (restart from intro)
 	}
-	path = path[1:] //remove the '/' in path
+	return path[1:] //remove the '/' in path
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 
 	//check if path name exists in the JSON file (e.g. '/new-york' would be a valid path URL as 'new-york' is a key to a Chapter in Story map)
 	if chapter, ok := h.s[path]; ok {
-		err := tpl.Execute(w, chapter) //http.ResponseWriter is an io.Writer => can use it in Template.Execute to write story chapter to response body
+		err := h.t.Execute(w, chapter) //http.ResponseWriter is an io.Writer => can use it in Template.Execute to write story chapter to response body
 		if err != nil {
 			log.Printf("%v", err)
 			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
